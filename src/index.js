dcf4e4107cbb3bced7ad7df08e5588de0f9b7562
Maria Kurakevich <marfysa14@gmail.com>
2021-08-19 14:22:24 +0300

    feat: add part solution

--- src/index.js
+++ src/index.js
@@ -9,10 +9,26 @@ function expressionCalculator(expr) {
     let result = expr.split((/\s+/g));
     let res = result.join('').split('');
     let num = 0;
-    //let arr = result.filter(e => e !== '').join(',');
-    console.log(res[1]);
+    let leftCount = 0;
+    let rightCount = 0;
+
+    for (let br of res) {
+        if (br === '('){
+            leftCount++;
+        }
+        if (br === ')') {
+            rightCount--;
+        }
+    }
+
+    if (leftCount !== rightCount) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    }
+
     for (let i = 0; i < res.length; i++) {
-        if (res[i] === '+') {
+        if (res[i] === 0 && res[i-1] === '/') {
+            throw new Error('TypeError: Division by zero.')
+        } else if (res[i] === '+') {
           let add = Number(res[i-1]) + Number(res[i+1]);
            return add;
         } else if (res[i] === '-') {
@@ -21,11 +37,14 @@ function expressionCalculator(expr) {
         } else if (res[i] === '*') {
            let mult = Number(res[i-1]) * Number(res[i+1]);
             return mult;
-    } else if (res[i] === '/') {
+        } else if (res[i] === '/') {
         let div = Number(res[i-1]) / Number(res[i+1]);
         return div;
+        } 
+    }
 
-}
+    for (let i = 0; i < res.length; i++){
+        
     }
 }
 module.exports = {
